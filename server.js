63078151b57e06b8230b30f58ab5c5e4dd39b543
Floris <weersproductions@gmail.com>
2018-11-15 21:08:27 +0100

    Add database stuff

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const config = require('./config');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const coffeeRoutes = require('./api/routes/coffeeRoutes');
+
+coffeeRoutes(app);
 
 const server = app.listen(config.get('PORT'), () => {
   const { port } = server.address();
