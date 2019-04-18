6397a5fc148387d69aa7fd5fdbd002e1598ce447
Floris <weersproductions@gmail.com>
2019-04-18 11:48:56 +0200

    Add socket

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const config = require('./config');
 
 const app = express();
+const httpServer = require('http').Server(app);
+const io = require('socket.io')(httpServer);
+const config = require('./config');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -11,7 +13,14 @@ const coffeeRoutes = require('./api/routes/coffeeRoutes');
 
 coffeeRoutes(app);
 
-const server = app.listen(config.get('PORT'), () => {
+const server = httpServer.listen(config.get('PORT'), () => {
   const { port } = server.address();
   console.log(`5-2 Unlimited RESTful API server started on: ${port}`);
+});
+
+io.on('connection', (socket) => {
+  socket.emit('news', { hello: 'world' });
+  socket.on('event', (data) => {
+    console.log(data);
+  });
 });
